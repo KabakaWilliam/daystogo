21c3c65cefdc3239b1634807a193d060df78edf5
William Gitta <73100027+KabakaWilliam@users.noreply.github.com>
2021-11-23 13:16:42 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+
 import './App.css';
 
+
+const calculateTimeLeft = () => {
+  // let year = new Date().getFullYear();
+  const difference = +new Date(`11/21/2022`) - +new Date();
+  // console.log( difference)
+
+  let timeLeft:{[key: string]: number} = {};
+
+  if (difference> 0){
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60)
+    };
+  }
+  return timeLeft
+};
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      setTimeLeft(calculateTimeLeft());
+      }, 1000);
+      return () => clearTimeout(timer);
+    });
+  
+
+  // const timerComponents = new Array< string>()
+  const timerComponents:any = []
+  
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    } 
+    timerComponents.push(
+      <span key = {Math.random()}>
+        {timeLeft[interval]} {interval} {"  "}
+      </span>
+    );
+  });
+    return (
+      <div className="wrapper">
+        <div className="heading">
+        {/* {console.log(timerComponents)} */}
+        <h1> Time Left till the World Cup </h1>
+        <div className="countWrapper">
+          {timerComponents.length ? timerComponents : <span>World Cup!</span>}
+        </div>
+
+      </div>
+      </div>
+    );
+  }
 
 export default App;
